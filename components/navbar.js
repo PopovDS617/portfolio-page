97167fa72a1850d5dd92ab6acc515d239700c46b
PopovDS617 <popovds.bm@gmail.com>
2022-09-25 01:01:21 +0300

    basic components

---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+import Logo from './logo';
+import NLink from 'next/link';
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValu
+} from '@chakra-ui/react';
+
+import { HamburgerIcon } from '@chakra-ui/icons';
+
+const LinkItem = ({ href, path, children }) => {};
