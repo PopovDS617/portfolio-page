9badac5f1db140459c9c821541a2411305f5db2e
PopovDS617 <popovds.bm@gmail.com>
2022-09-25 01:00:45 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Привет, это страничка
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Дмитрий Попов
+          </Heading>
+          <p>разработчик из России</p>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Page;
