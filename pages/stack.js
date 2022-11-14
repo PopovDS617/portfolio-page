434f10f0063ab983146925b6583d47c624c7c301
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-14 14:30:46 +0300

    Update stack.js

--- pages/stack.js
+++ pages/stack.js
@@ -1,1 +1,54 @@
 
+ 
+import {
+  Container,
+  Box,
+  Heading,
+ 
+  useColorModeValue,
+ 
+ 
+  
+  
+  Icon
+} from '@chakra-ui/react';
+import Section from '../components/Section';
+import { Paragraph } from '../components/Paragraph';
+ 
+import ArticleLayout from '../components/layouts/ArticleLayout';
+ 
+ 
+ 
+
+const StackPage = () => {
+  return (
+    <ArticleLayout>
+      <Container>
+       
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              мой стек
+            </Heading>
+            <p>мой стек</p>
+          </Box>
+         
+        </Box>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            стек
+          </Heading>
+          <Paragraph>
+            Hello this is mine description hello and welcome
+          </Paragraph>
+         
+        </Section>
+        
+      </Container>
+      <Footer />
+    </ArticleLayout>
+  );
+};
+
+export default StackPage;
+export { getServerSideProps } from '../components/ssrSetup';
