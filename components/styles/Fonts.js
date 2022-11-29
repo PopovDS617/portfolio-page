fc37e07045448d5e7622ef5b3ea4b0d24cc2a0d2
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2022-11-29 14:38:44 +0300

    Update Fonts.js

--- components/styles/Fonts.js
+++ components/styles/Fonts.js
@@ -2,7 +2,7 @@ import { Global } from '@emotion/react';
 
 const Fonts = () => (
   <Global
-    styles={`@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap')`}
+    styles={`@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans&family=IBM+Plex+Serif&display=swap');`}
   />
 );
 
