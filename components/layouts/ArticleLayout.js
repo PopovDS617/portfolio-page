50ea60c5819659da74511036535a139dbf25e493
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2023-01-17 11:39:42 +0300

    Update ArticleLayout.js

--- components/layouts/ArticleLayout.js
+++ components/layouts/ArticleLayout.js
@@ -5,7 +5,7 @@ import { GridItemStyle } from '../grid/GridItem';
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: 20 }
+  exit: { opacity: 0, x: -0, y: 20 }
 };
 
 const ArticleLayout = ({ children, title }) => (
