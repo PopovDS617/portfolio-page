21e17d70a75b49e725bb6e77e3371cd599cfc9dd
PopovDS617 <popovds.bm@gmail.com>
2023-01-28 12:20:28 +0300

    fear: add stack page, add code snippet component

--- components/CodeSnippet.js
+++ components/CodeSnippet.js
@@ -1,10 +1,15 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import prettier from 'prettier';
+import parserBabel from 'prettier/parser-babel';
 
 const CodeSnippet = props => {
   return (
     <SyntaxHighlighter language="javascript" style={vscDarkPlus}>
-      {props.children}
+      {prettier.format(props.children, {
+        parser: 'babel',
+        plugins: [parserBabel]
+      })}
     </SyntaxHighlighter>
   );
 };
--- pages/stack.js
+++ pages/stack.js
@@ -1,24 +1,9 @@
-import NextLink from 'next/link';
-import {
-  Container,
-  Box,
-  Heading,
-  Image,
-  useColorModeValue,
-  Button,
-  Link,
-  List,
-  ListItem,
-  Icon,
-  ListIcon,
-  UnorderedList
-} from '@chakra-ui/react';
+import { Container, Heading } from '@chakra-ui/react';
 import Section from '../components/Section';
 import { Paragraph } from '../components/styles/Paragraph';
-import { ChevronRightIcon, SearchIcon } from '@chakra-ui/icons';
-import { BioSection, BioYear } from '../components/styles/Bio';
+
 import ArticleLayout from '../components/layouts/ArticleLayout';
-import { BsTelegram, BsGithub } from 'react-icons/bs';
+
 import useTranslation from 'next-translate/useTranslation';
 
 const StackPage = () => {
@@ -27,91 +12,37 @@ const StackPage = () => {
   return (
     <ArticleLayout>
       <Container>
-        <Box display={{ md: 'flex' }}>
-          
-            <Heading as="h2" variant="page-title" textAlign={{ sm: 'justify' }}>
-              {t('title')}
-            </Heading>
-            <Paragraph>разработчик из России</Paragraph>
-          </Box>
-          <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
-            textAlign="center"
-          >
-           
         <Section delay={0.1}>
           <Heading as="h3" variant="section-title">
-            Work
+            front
           </Heading>
           <Paragraph fontSize={20}>
             Hello this is mine description hello and welcome
           </Paragraph>
-          
         </Section>
-        { <Section delay={0.2}>
+        <Section delay={0.2}>
           <Heading as="h3" variant="section-title">
-            <BioSection>
-              <BioYear>2000</BioYear>
-            </BioSection>
+            back
           </Heading>
-        </Section> }
-        <Section delay={0.3}>
-          <Heading as="h3" variant="section-title">
-            {t('hobbiesTitle')}
-          </Heading>
-
-          <List fontSize={20}>
-            <ListItem>
-              <ListIcon
-                as={SearchIcon}
-                color={useColorModeValue(
-                  'customDarkBlueColor',
-                  'customSaturatedGreenColor'
-                )}
-              />
-              {t('hobbie1')}
-            </ListItem>
-            <ListItem>
-              <ListIcon
-                as={SearchIcon}
-                color={useColorModeValue(
-                  'customDarkBlueColor',
-                  'customSaturatedGreenColor'
-                )}
-              />
-              {t('hobbie2')}
-            </ListItem>
-            <ListItem>
-              <ListIcon
-                as={SearchIcon}
-                color={useColorModeValue(
-                  'customDarkBlueColor',
-                  'customSaturatedGreenColor'
-                )}
-              />
-              {t('hobbie3')}
-            </ListItem>
-            <ListItem>
-              <ListIcon
-                as={SearchIcon}
-                color={useColorModeValue(
-                  'customDarkBlueColor',
-                  'customSaturatedGreenColor'
-                )}
-              />
-              {t('hobbie4')}
-            </ListItem>
-          </List>
+          <Paragraph fontSize={20}>
+            Hello this is mine description hello and welcome
+          </Paragraph>
         </Section>
         <Section delay={0.3}>
           <Heading as="h3" variant="section-title">
-            {t('contactsTitle')}
+            db
           </Heading>
-        
-             
-             
+          <Paragraph fontSize={20}>
+            Hello this is mine description hello and welcome
+          </Paragraph>
+        </Section>
+        <Section delay={0.4}>
+          <Heading as="h3" variant="section-title">
+            to do
+          </Heading>
+          <Paragraph fontSize={20}>
+            Hello this is mine description hello and welcome
+          </Paragraph>
         </Section>
       </Container>
     </ArticleLayout>
