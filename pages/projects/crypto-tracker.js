f11bd96bb5af4ba7e3b1dbcbbd7b6dceb9215145
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2023-02-01 14:33:45 +0300

    Update crypto-tracker.js

--- pages/projects/crypto-tracker.js
+++ pages/projects/crypto-tracker.js
@@ -25,7 +25,7 @@ const Project = () => {
           </ListItem>
           <ListItem>
             <ProjectMeta>Stack</ProjectMeta>
-            <span>TypeScript, React, CSS</span>
+            <span>Typescript, React, CSS</span>
           </ListItem>
           <ListItem>
             <ProjectMeta>Source</ProjectMeta>
