d5fa039c53318e61022746f6faf1bf7b3cdf4900
PopovDS617 <popovds.bm@gmail.com>
2023-02-06 18:33:31 +0300

    feat: 3d model animation slows down at the most right and the most left positions

--- components/3dmodel/face-model.js
+++ components/3dmodel/face-model.js
@@ -1,27 +1,45 @@
 import React, { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { angleToRadians } from '../../lib/angleToRadians';
 
 export function Model(props) {
   const modelRef = useRef();
-  let moveRight = true;
+  if (modelRef.current) {
+    modelRef.current.rotation.moveRight = true;
+  }
 
   useFrame(({}) => {
     if (modelRef.current.rotation.y >= 1.4) {
-      console.log('yay');
-      moveRight = false;
+      modelRef.current.rotation.moveRight = false;
     }
 
     if (modelRef.current.rotation.y <= 0.18) {
-      moveRight = true;
+      modelRef.current.rotation.moveRight = true;
     }
 
-    if (modelRef.current.rotation && moveRight) {
+    if (
+      modelRef.current.rotation &&
+      modelRef.current.rotation.moveRight &&
+      modelRef.current.rotation.y > 1.3
+    ) {
+      modelRef.current.rotation.y += 0.002;
+    } else if (
+      modelRef.current.rotation &&
+      modelRef.current.rotation.moveRight
+    ) {
       modelRef.current.rotation.y += 0.0033;
     }
 
-    if (modelRef.current.rotation && !moveRight) {
+    if (
+      modelRef.current.rotation &&
+      !modelRef.current.rotation.moveRight &&
+      modelRef.current.rotation.y < 0.3
+    ) {
+      modelRef.current.rotation.y -= 0.002;
+    } else if (
+      modelRef.current.rotation &&
+      !modelRef.current.rotation.moveRight
+    ) {
       modelRef.current.rotation.y -= 0.0033;
     }
   });
