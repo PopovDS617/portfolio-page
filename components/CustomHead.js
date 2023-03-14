02fadbc2544f20b5a99dd3ee1a5b5688c5db8ed1
PopovDS617 <popovds.bm@gmail.com>
2023-03-14 17:19:41 +0300

    add y and g metatags

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -8,7 +8,11 @@ const CustomHead = () => {
       <meta name="author" content="Dmitry Popov" />
       <meta name="author" content="Дмитрий Попов" />
       <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-
+      <meta name="yandex-verification" content="b4595331cb9305bc" />
+      <meta
+        name="google-site-verification"
+        content="zSjWpAENkeNVCbUW0wcUn7nMovtl-uBNrEyeTVpCtE0"
+      />
       <meta property="og:site_name" content="Дмитрий Попов" />
       <meta name="og:title" content="Дмитрий Попов" />
       <meta property="og:type" content="website" />
