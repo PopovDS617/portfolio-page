94d70a6215eff573faef3a71bce44aa71a82b64a
Popov Dmitry <popovds.bm@gmail.com>
2024-04-17 17:48:28 +0300

    chore: change yandex verification metatag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class Document extends NextDocument {
         <Head />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <meta name="yandex-verification" content="6e68aa82a134b6de" />
+        <meta name="yandex-verification" content="069f11408e51a1b9" />
         <meta name="google-site-verification" content="Cp90cSWW3ZoFKAAkBymkNnkMdbZ2NnuD9jvEQ3yKZvs" />
         <body style={{ overflow: 'overlay' }}>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
